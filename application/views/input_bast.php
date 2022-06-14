7b5cb24eee6d5ec7257cf1b125e7686f572d02b4
Jasen Aprian <46961710+jazen4p@users.noreply.github.com>
2022-06-14 07:55:26 +0700

    Delete application directory

--- application/views/input_bast.php
+++
@@ -1,3 +0,0 @@
-<html>
-    <a href="<?php echo base_url()?>Dashboard/add_input_bast">generate</a>
-</html>
--- application/views/kasir/include/fixedtop.php
+++
@@ -1,2 +0,0 @@
-
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
